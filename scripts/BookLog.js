e475083f98fd50df89eb6ee200f6ee065adaef4c
Greg Korte <gregmkorte@gmail.com>
2023-05-07 00:13:30 -0500

    Deleting unneeded files

--- scripts/BookLog.js
+++
@@ -1,6 +0,0 @@
-// HTML ex... "${child} checked out a ${genre} titled ${book}"
-    // need multiple loops to access a genre on a book
-    export const BookLog = async () => {
-        const response = await fetch("http://localhost:8088/bookLogs")
-        return await response.json()
-    }
--- scripts/Children.js
+++
@@ -1,4 +0,0 @@
-export const Children = async () => {
-    const response = await fetch("http://localhost:8088/children")
-    return await response.json()
-}
